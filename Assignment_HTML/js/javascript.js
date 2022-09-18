d217106648f0134be7378574b12265628403b1f5
Sumit Kaila <sumitkaila@IN-C02DQAHMP3XY.bbrouter>
2022-09-18 18:00:02 +0530

    added code

---
+++ Assignment_HTML/js/javascript.js
@@ -0,0 +1,19 @@
+// console.log('hello this is from JS file') 
+
+
+
+
+// @param a, b = this is the defination of a and b
+
+// 
+// 
+// .
+// sdfjksa9876865687rdyt///sdfsdfiughjkasd
+//////////uiyasgdkahskjdhaksjdhk
+//
+
+    // i 
+    //  will
+    //  Comment
+    //  multiline
+    //  here
